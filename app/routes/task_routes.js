4f4772a5cdd2dddc2a46b91344ca75b862e0a310
furevor <furevor@gmail.com>
2018-02-22 15:53:43 +0800

    heh.. let's try

--- app/routes/task_routes.js
+++ app/routes/task_routes.js
@@ -1,18 +1,8 @@
 var ObjectID = require('mongodb').ObjectID;
+var http = require('http');
 var request = require('request');
 
-function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
-}
-
-
 module.exports = function(app, db) {
-
-	//app.get('*/', function(req, res) {
-	//  res.sendFile(path.join(__dirname + '/public/index.html'));
-	//});*/
-
 
     app.get('/api/tasks', (req, res) => {
 
@@ -30,12 +20,6 @@ module.exports = function(app, db) {
             return item;
         });
 
-
-
-        //var jsonArray1 = [{ _id: '13', heading: 'third is just planing...', priority: 1, state: false }, { _id: '12', heading: 'second note.. in process..', priority: 1, state: false }];
-        //var jsonArray2 = [{ _id: '12', heading: 'second note.. in process..', priority: 1, state: false }];
-
-
         /*
         cursor.each((err, item) => {
             if (err) {
@@ -51,6 +35,67 @@ module.exports = function(app, db) {
         //res.send(cursor);
 
     });
+
+    var secretstring = "";
+    var tempRes = "";
+
+    app.get('/', (req, res) => {
+    /*
+        if(req.query.code != undefined && req.query.state != undefined) {
+            if(req.query.state == "lunartemple2112")
+            {
+                secretstring = req.query.code;
+                console.log("success!!");
+
+
+                var bodyTest = {
+                    heading: 'some text.. please...',
+                    priority: 0,
+                    state: false,
+                }
+
+                var bodyData = {
+                    client_id: '5b2714d62ded4a8dbc11cd22cdb5cb87',
+                    client_secret: '1d8df6f955344f6f86b299d88a91b0cc',
+                    code: secretstring,
+                    redirect_uri: 'https://asptodo-2049.herokuapp.com/'
+                }
+
+                updateClient(bodyData, res);
+                //res.send('{' + tempRes + '}');
+                console.log('some assy fucking sheet...' + tempRes);
+                //res.send(tempRes);
+            }
+        }
+        else
+        {
+            console.log('heheheh');
+        }
+
+    */
+    });
+
+
+    function updateClient(postData){
+        var clientServerOptions = {
+            uri: 'https://todoist.com/oauth/access_token',
+            body: JSON.stringify(postData),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        request(clientServerOptions, function (error, response) {
+            console.log(error, response.body);
+
+            tempRes = JSON.parse(response.body);
+            console.log(tempRes.access_token);
+
+            return;
+        });
+
+
+    }
 
     /*
     //метод, что призван отдавать note по id!!
@@ -70,56 +115,6 @@ module.exports = function(app, db) {
         });
     });
      */
-	 
-	 
-	var secretstring = "";
-
-	var tempRes = "";
-	
-    app.get('/', (req, res) => {
-
-        if(req.query.code != undefined && req.query.state != undefined) {
-            if(req.query.state == "lunartemple2112")
-            {
-                secretstring = req.query.code;
-                console.log("success!!");
-
-                var bodyData = {
-                    client_id: '5b2714d62ded4a8dbc11cd22cdb5cb87',
-					client_secret: '1d8df6f955344f6f86b299d88a91b0cc',
-					code: secretstring,
-					redirect_uri: 'https://asptodo-2049.herokuapp.com/'
-                }
-				
-				tempRes = updateClient(bodyData);
-				res.send('{' + tempRes + '}');
-
-                
-            }
-        }
-        else
-        {
-            console.log('heheheh');
-        }
-
-
-    });
-
-
-    function updateClient(postData){
-        var clientServerOptions = {
-            uri: 'https://todoist.com/oauth/access_token',
-            body: JSON.stringify(postData),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-        return request(clientServerOptions, function (error, response) {
-            console.log(error, response.body);
-            return response.body;
-        });
-    } 
 
     app.delete('/api/tasks/:id', (req, res) => {
 
@@ -138,7 +133,6 @@ module.exports = function(app, db) {
     app.post('/api/tasks', (req, res) => {
         const note = { heading: req.body.heading, priority: req.body.priority, state: req.body.state };
         console.log(req.body);
-		console.log(tempRes);
         db.collection('angulartasks').insert(note, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
@@ -161,6 +155,68 @@ module.exports = function(app, db) {
             }
         });
     });
-};
 
-// 5a80ffa17e6f9c220c09097c
+
+
+    app.post('/api/access', (req, res, next) => {
+
+        var scode = req.body.secretCode;
+
+        var bodyData = {
+            client_id: '5b2714d62ded4a8dbc11cd22cdb5cb87',
+            client_secret: '1d8df6f955344f6f86b299d88a91b0cc',
+            code: scode,
+            redirect_uri: 'https://asptodo-2049.herokuapp.com/'
+        }
+
+        var clientServerOptions = {
+            uri: 'https://todoist.com/oauth/access_token',
+            body: JSON.stringify(bodyData),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        request(clientServerOptions, function (error, response) {
+            console.log(error, response.body);
+
+            tempRes = JSON.parse(response.body);
+            console.log(tempRes.access_token);
+            // по идее нужно добавить условие, чтобы отправлять разлинчые статусы в случае успеха или нет!!!!!
+            // всегда будет возвращаться 200...
+            next();
+            return;
+        });
+
+
+    }, (req, res) => {
+        res.sendStatus(200);
+    });
+
+
+    app.get('/api/import', (req, res) => {
+        console.log('hello from here!!!');
+        var options = {
+            url: 'https://todoist.com/api/v7/sync',
+            headers: {
+                'token': tempRes,
+                'sync_token': '*',
+                'resource_types': '["projects"]'
+
+            }
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                console.log(info.stargazers_count + " Stars");
+                console.log(info.forks_count + " Forks");
+            }
+        }
+
+        request(options, callback);
+    });
+
+
+
+};
